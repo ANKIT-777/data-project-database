5c0750479e4bf641667295e5331e187a2fd8c39c
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-26 17:18:44 +0530

    new updates

--- SqlAlchemy/Schema.py
+++ SqlAlchemy/Schema.py
@@ -1,28 +1,63 @@
-from sqlalchemy import create_engine, Column, String, Integer, Boolean
+from sqlalchemy import create_engine, Column, String, Integer, Boolean,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
+
+
 Base = declarative_base()
+
 username = "ankit69"
 password = "ankit69"
-hostname = "sample"  # Replace with your PostgreSQL server's hostname or IP address
+hostname = "localhost"  # Replace with your PostgreSQL server's hostname or IP address
 port = "5432"  # Replace with the port where PostgreSQL is running (usually 5432)
-database_name = "postgres"  # Replace with the name of your PostgreSQL database
+database_name = "ipls"  # Replace with the name of your PostgreSQL databaseSchema
 
 # Create the SQLAlchemy engine 
-engine = create_engine(
-    f"postgresql://{username}:{password}@localhost:{port}/{database_name}", echo=True
-)
+# Create the SQLAlchemy engine
+try:
+    engine = create_engine(f"postgresql://{username}:{password}@{hostname}:{port}/{database_name}", echo=True)
+except Exception as e:
+    print("Error creating the engine:", e)
+
+try:
+    engine = create_engine(f"postgresql://{username}:{password}@{hostname}:{port}/{database_name}", echo=True)
+except Exception as e:
+    print("Error creating the engine:", e)
+
+
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+class Matches(Base):
+    __tablename__ = "Matches"
+    id = Column(Integer, primary_key=True)
+    match_id = Column(Integer,unique=True)
+    team_name = Column(String)
+    season = Column(String)
+    city = Column(String)
+    date = Column(String)  # Assuming date is a string
+    team1 = Column(String)
+    team2 = Column(String)
+    toss_winner = Column(String)
+    toss_decision = Column(String)
+    result = Column(String)
+    dl_applied = Column(Integer)
+    winner = Column(String)
+    win_by_runs = Column(Integer)
+    win_by_wickets = Column(Integer)
+    player_of_match = Column(String)
+    venue = Column(String)
+    umpire1 = Column(String)  
+    umpire2 = Column(String)
+    umpire3 = Column(String)
 
-class IPL(Base):
-    __tablename__ = "Cricket_match"
-
-    match_id = Column("match_id", Integer, primary_key=True)
+class Deliveries(Base):
+    __tablename__ = "Deliveries"
+    
+    id = Column(Integer, primary_key=True)
+    match_id = Column("match_id", Integer, ForeignKey("Matches.match_id"))
     inning = Column("inning", Integer)
     batting_team = Column("batting_team", String)
     bowling_team = Column("bowling_team", String)
@@ -44,44 +79,13 @@ class IPL(Base):
     dismissal_kind = Column("dismissal_kind", String)
     fielder = Column("fielder", String)
 
-    def __repr__(self):
-        return f"<IPL match_id={self.match_id}, inning={self.inning}, batting_team={self.batting_team}, bowling_team={self.bowling_team}>"
+    
+class Umpire(Base):
+    __tablename__ = "Umpires"
+    id = Column(Integer, primary_key=True)
+    umpire_name = Column("umpire_name", String)
+    country_name = Column("country_name", String)
 
 
 Base.metadata.create_all(engine)
 
-# Sample data (you can load your data from a file or another source)
-
-data = [
-    {
-        "match_id": 1,
-        "inning": 1,
-        "batting_team": "Sunrisers Hyderabad",
-        "bowling_team": "Royal Challengers Bangalore",
-        "over": 1,
-        "ball": 1,
-        "batsman": "DA Warner",
-        "non_striker": "S Dhawan",
-        "bowler": "TS Mills",
-        "is_super_over": 0,
-        "wide_runs": 0,
-        "bye_runs": 0,
-        "legbye_runs": 0,
-        "noball_runs": 0,
-        "penalty_runs": 0,
-        "batsman_runs": 0,
-        "extra_runs": 0,
-        "total_runs": 0,
-        "player_dismissed": None,
-        "dismissal_kind": None,
-        "fielder": None,
-    },
-    # Add more rows here...
-]
-
-# Use bulk_insert_mappings to insert data
-session.bulk_insert_mappings(IPL, data)
-
-# Commit all the changes to the database
-session.commit()
- 
